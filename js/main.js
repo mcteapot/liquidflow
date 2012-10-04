50931c097083f74e8149347b7ab25f96200123d0
Arjun Prakash <archieoi@gmail.com>
2012-10-04 00:01:29 -0700

    video drawn into canvas tag.

--- js/main.js
+++ js/main.js
@@ -2,48 +2,73 @@
 $("document").ready(function() {
 
 	//alert("alerts suck");
+	/* GET VIDEO */
+	var video = $('#output_video')[0];
 
-	var n = navigator,
-	is_webkit = false;
+	var webcamError = function(e) {
+		alert('Webcam error!', e);
+	};
 
-	if (n.getUserMedia) {
-    	// opera users (hopefully everyone else at some point)
-    	n.getUserMedia({video: true, audio: true}, onSuccess, onError);
-		console.log("opera user");
-	}
-	else if (n.webkitGetUserMedia) {
-    // webkit users
-    	is_webkit = true;
-    	n.webkitGetUserMedia({audio:true, video:true}, onSuccess, onError);
-		console.log("chrome user");
-	}
-	else {
-    	// moms, dads, grandmas, and grandpas
-	}
- 
-	function onSuccess(stream) {
-	    var output = document.getElementById('output'), // a video element
-	    source;
-	 
-	    output.autoplay = true; // you can set this in your markup instead
-	 
-	    if (!is_webkit) {
-	        source = stream;
-	    }
-	    else {
-	        source = window.webkitURL.createObjectURL(stream);
-	    }
-	 
-	    output.src = source;
+	if (navigator.getUserMedia) {
+		navigator.getUserMedia({audio: true, video: true}, function(stream) {
+			video.src = stream;
+			video.autoplay = true;
+			initialize();
+		}, webcamError);
+	} else if (navigator.webkitGetUserMedia) {
+		navigator.webkitGetUserMedia({audio:true, video:true}, function(stream) {
+			video.src = window.webkitURL.createObjectURL(stream);
+			video.autoplay = true;
+			initialize();
+		}, webcamError);
+	} else {
+		//video.src = 'somevideo.webm'; // fallback.
 	}
 
-	function onError() {
-	    // womp, womp =(
+	/* UTLITES */
+
+	window.requestAnimFrame = (function(){
+		return  window.requestAnimationFrame       ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+			function( callback ){
+				window.setTimeout(callback, 1000 / 60);
+			};
+	})();
+
+	/* DRAWING TO CANVAS */
+	var timeOut, lastImageData;
+	var canvasSource = $('#canvas_video')[0];
+	var canvasBlended = $('#canvas_video_blend')[0];
+	var contextSource = canvasSource.getContext('2d');
+	var contextBlended = canvasBlended.getContext('2d');
+	var soundContext, bufferLoader;
+
+	contextSource.translate(canvasSource.width, 0);
+	contextSource.scale(-1, 1);
+
+	function initialize() {
+		update();
 	}
+
+	function update() {
+		drawVideo();
+		//blend();
+		//checkAreas();
+		requestAnimFrame(update);
+		//window.webkitRequestAnimationFrame(update);
+		//timeOut = setTimeout(update, 1000/60);
+	}
+
+	function drawVideo() {
+		//contextSource.drawImage(video, 0, 0, video.width, video.height);
+		contextSource.drawImage(video, 0, 0, video.width, video.height);
+		//console.log('workign');
+	}
+
+	
 
 
 });
-
-
-
-
